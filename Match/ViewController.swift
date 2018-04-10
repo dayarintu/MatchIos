2b557e3ffd1f88626b1ce5fec165da2e18d7aafa
dhaya <dhayareddy89@gmail.com>
2018-04-09 22:07:40 -0400

    Initial Commit

---
+++ Match/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Match
+//
+//  Created by Dayakar Reddy on 4/9/18.
+//  Copyright © 2018 Dayakar Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+        
+    }
+    func flipCard(withEmoji emoji: String , on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle(" ", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+}
+
+
+
